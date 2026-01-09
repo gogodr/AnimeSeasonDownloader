42b46e4d26e88879b3ceae86f89f8a350f6d17ba
Gogodr <gogodr@hotmail.com>
2026-01-08 19:42:48 -0500

    setMaxListeners infinity to allow stream of torrents

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ const __dirname = dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+process.setMaxListeners(Infinity);
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+});
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    process.exit(1);
+});
+
 // Middleware
 app.use(express.json());
 app.use(express.static(join(__dirname, 'dist')));
